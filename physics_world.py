5fd63811791a56909c3398371ecaf462500174a4
sozho <donalexxxandr@gmail.com>
2021-01-18 17:49:24 +1000

    Update physics_world.py
    - Added Collision Check

--- physics_world.py
+++ physics_world.py
@@ -12,13 +12,13 @@ class physics_world:
     def resolve_groups(self):
         self.groups.clear()
         the_last_index = self.objects.__len__()
-        for i in range(0, the_last_index - 1):
-            for j in range(i + 1, the_last_index):
-                self.groups.append([i, j])
+        for i in range(0, the_last_index - 2):
+            for j in range(i + 1, the_last_index - 1):
+                    self.groups.append([i, j, (objects[i].radius + objects[j].radius)^2])
 
     def step(self, surface):
 
-        # Applyinh forces
+        # Applying forces
         for object in self.objects:
             object.velocity += object.force / object.mass * self.delta_time
             object.position += object.velocity * self.delta_time
@@ -29,8 +29,13 @@ class physics_world:
         distance_vector = Vector2D(0, 0)
         for group in self.groups:
             distance_vector = self.objects[group[1]].position - self.objects[group[0]].position
+            distance_squared = distance_vector.length_squared()
+            if distance_squared <= self.objects[group[3]]:
+                self.objects[group[0]].on_collision(self.objects[group[1]])
+                self.objects[group[1]].on_collision(self.objects[group[0]])
+
             current_force = distance_vector.normalized() * (self.objects[group[0]].mass * self.objects[
-                group[1]].mass) / distance_vector.length_squared() * self.G
+            group[1]].mass) / distance_squared * self.G
             self.objects[group[0]].force += current_force
             self.objects[group[1]].force += -current_force
 
